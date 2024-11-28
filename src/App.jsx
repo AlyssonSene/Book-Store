6d5f896399708456f1b28f631155ae01e0524bc3
Alysson Sene <alyssonsene@outlook.com>
2024-11-28 14:43:37 -0300

    ADD: Configurando projeto, adicionando redux, router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import BooksPage from './views/BooksPage.jsx';
-import SingleBookPage from './views/SingleBookPage.jsx';
-import AddBookPage from './views/AddBookPage.jsx';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddBookPage from "./views/AddBookPage.jsx";
+import BooksPage from "./views/BooksPage.jsx";
+import SingleBookPage from "./views/SingleBookPage.jsx";
 
 function App() {
-
   return (
     <>
-      <BooksPage />
-        
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<BooksPage />} />
+          <Route path="/add-book" element={<AddBookPage />} />
+          <Route path="/book/:id" element={<SingleBookPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
